d4b480a2d38ff5466daa4f47be4482512bc87ad8
uo270411 <uo270411@uniovi.es>
2021-02-14 13:57:42 +0100

    SDI-2021-1008-2.1-Create Spring project.

---
+++ sdi2021-1008-spring/src/main/java/com/uniovi/Sdi20211008SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi20211008SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi20211008SpringApplication.class, args);
+	}
+
+}
---
+++ sdi2021-1008-spring/src/test/java/com/uniovi/Sdi20211008SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi20211008SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
