7859ecab14acb1cd187870facb23d18790b66700
uo270411 <uo270411@uniovi.es>
2021-03-05 17:02:38 +0100

    Comienzo del proyecto practica 1.

---
+++ sdi-entrega1-2021-1008-2021-1014/src/test/java/com/uniovi/SdiEntrega12021100820211014ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdiEntrega12021100820211014ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
